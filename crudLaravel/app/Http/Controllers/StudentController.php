1cd4fc5928cf3f52c2807d856006c18efd0db36c
rezimaulana <rsazrm@gmail.com>
2022-08-29 20:07:41 +0700

    make get all student

--- crudLaravel/app/Http/Controllers/StudentController.php
+++ crudLaravel/app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
@@ -14,6 +15,8 @@ class StudentController extends Controller
     public function index()
     {
         //
+        $studentData = Student::all();
+        return view('contents/student', ['student' => $studentData]);
     }
 
     /**
--- crudLaravel/resources/views/contents/student.blade.php
+++ crudLaravel/resources/views/contents/student.blade.php
@@ -1,21 +1,39 @@
 @extends('layouts.app')
 @section('content')
-<h3>Stundent</h3>
-<table class="table table-striped">
-    </tr>
-        <th>Name</th>
-        <th>NIS</th>
-        <th>DOB</th>
-    </tr>
-    <tr>
-        <td>Rezi</td>
-        <td>10515258</td>
-        <td>10-12-96</td>
-    </tr>
-    <tr>
-        <td>Rezi</td>
-        <td>10515258</td>
-        <td>10-12-96</td>
-    </tr>
-</table>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>Data Siswa</h3>
+        </div>
+        <div class="card-body">
+            <a href="{{ route('student.create') }}" class="btn btn-info">Tambah Data Siswa</a>
+            <table class="table table-bordered table-striped mt-4">
+                <tr>
+                    <th>Name</th>
+                    <th>Student ID</th>
+                    <th>Date of Birth</th>
+                    <th>Action</th>
+                </tr>
+                @foreach($student as $s)
+                <tr>
+                    <td>{{ $s->name }}</td>
+                    <td>{{ $s->studentId }}</td>
+                    <td>{{ $s->dob }}</td>
+                    <td>
+                        <ul class="nav">
+                            <a href="{{ route ('student.show', $s->id) }}" class="btn btn-success mr-2">Show</a>
+                            <a href="{{ route ('student.edit', $s->id) }}" class="btn btn-primary mr-2">Edit</a>
+                            <form action="{{ route ('student.destroy', $s->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </ul>
+                    </td>
+                </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
+</div>
 @endsection
