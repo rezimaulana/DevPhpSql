c607fdcdb2d1922c538012e46d040598917fcdcc
rezimaulana <rsazrm@gmail.com>
2022-08-29 15:35:56 +0700

    students migrate and seed

---
+++ crudLaravel/database/seeders/studentsSeed.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class studentsSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('students')->insert([
+            'name' => 'Ghani Rizqan',
+            'studentId' => '101',
+            'dob' => '1998-10-09'
+        ]);
+        DB::table('students')->insert([
+            'name' => 'Rifqi Naufal',
+            'studentId' => '102',
+            'dob' => '1998-11-11'
+        ]);
+    }
+}
